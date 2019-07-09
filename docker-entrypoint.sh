f6120adc471eb72b196f57f04015dcab44cba737
Michael Bøje Skjærlund <Michael_b_hansen@live.dk>
2019-07-09 15:22:01 +0200

    testing

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 echo "Entry point script is now working !"
 
+apk add php7
+
 if [ ! -z ${CRON_JOB_DEPENDENCIES_ARG}]; then
     export ${CRON_JOB_DEPENDENCIES}=${CRON_JOB_DEPENDENCIES_ARG}
     echo "The package to install is: ${CRON_JOB_DEPENDENCIES}"

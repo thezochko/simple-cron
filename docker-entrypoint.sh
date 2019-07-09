96d8fe7078379fa167972ea98b7df22d78c295f4
Michael Bøje Skjærlund <Michael_b_hansen@live.dk>
2019-07-09 15:10:08 +0200

    testing

---
+++ docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "Entry point script is now working !"
+
+if [ ! -z ${CRON_JOB_DEPENDENCIES_ARG}]; then
+    export ${CRON_JOB_DEPENDENCIES}=${CRON_JOB_DEPENDENCIES_ARG}
+    echo "The package to install is: ${CRON_JOB_DEPENDENCIES}"
+fi
+
+exec "$@"
